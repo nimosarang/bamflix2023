2c66ca6f73a6fa504514d847b9c946077c04c33d
nimosarang <111734043+nimosarang@users.noreply.github.com>
2023-08-28 23:55:34 +0900

    first commit

---
+++ src/main/java/com/bamflix/BamflixApplication.java
@@ -0,0 +1,13 @@
+package com.bamflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BamflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BamflixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bamflix/BamflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BamflixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
