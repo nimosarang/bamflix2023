a7eef5773663756085ab3b9791ea61c10a243bae
ekgml4573 <dh4573@naver.com>
2023-08-30 20:22:11 +0900

    파일 업로드 추가

--- src/main/java/com/bamflix/domain/content/ContentController.java
+++ src/main/java/com/bamflix/domain/content/ContentController.java
@@ -1,10 +1,14 @@
 package com.bamflix.domain.content;
 
+
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -14,23 +18,27 @@ public class ContentController {
 
     private final ContentService contentService;
 
+    private Logger logger = LoggerFactory.getLogger(ContentController.class);
+
     @GetMapping("/detail")
     public String getList(Model model) {
         List<ContentResponse> list = contentService.getList();
-
         model.addAttribute("list", list);
 
         return "content/detail";
     }
 
     @GetMapping("/write")
-    public String write() {
+    public String write(Model model, ContentRequest contentRequest) {
+        model.addAttribute("contentRequest", contentRequest);
         return "content/write";
     }
 
+
     @PostMapping("/save")
-    public String save(ContentRequest params) {
-        contentService.save(params);
+    public String save(ContentRequest contentRequest,@RequestParam MultipartFile file) {
+
+        contentService.save(contentRequest,file);
 
         return "redirect:detail";
     }
